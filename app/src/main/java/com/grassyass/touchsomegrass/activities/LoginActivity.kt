6fd6e71fe97b8208b0fee3e64736eb4b7f647302
Anthony Aromin <menekoiskawaii@gmail.com>
2022-09-24 00:27:29 +0800

    Add empty activity templates
    - OnboardingActivity
    - LoginActivity
    - SignUpActivity
    - HomeActivity

--- app/src/main/java/com/grassyass/touchsomegrass/activities/LoginActivity.kt
+++ app/src/main/java/com/grassyass/touchsomegrass/activities/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.grassyass.touchsomegrass
+package com.grassyass.touchsomegrass.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.grassyass.touchsomegrass.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
---
+++ app/src/main/java/com/grassyass/touchsomegrass/activities/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.grassyass.touchsomegrass.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.grassyass.touchsomegrass.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
