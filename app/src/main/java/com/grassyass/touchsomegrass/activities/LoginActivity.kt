b1dc669f3c9c8f691d184a37dd31b02e52336d8d
Anthony Aromin <menekoiskawaii@gmail.com>
2022-09-30 13:03:00 +0800

    Add Login feature

--- app/src/main/java/com/grassyass/touchsomegrass/activities/LoginActivity.kt
+++ app/src/main/java/com/grassyass/touchsomegrass/activities/LoginActivity.kt
@@ -1,12 +1,50 @@
 package com.grassyass.touchsomegrass.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseUser
 import com.grassyass.touchsomegrass.R
+import com.grassyass.touchsomegrass.network.Authentication
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var emailInputField: EditText
+    private lateinit var passwordInputField: EditText
+    private lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        initializeUI()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser: FirebaseUser? = Authentication.getCurrentUser()
+        if (currentUser != null) { navigateToHome() }
+    }
+
+    private fun initializeUI() {
+        emailInputField = findViewById(R.id.email_input_field)
+        passwordInputField = findViewById(R.id.password_input_field)
+        loginButton = findViewById(R.id.login_button)
+
+        loginButton.setOnClickListener { onLoginButtonPressed() }
+    }
+
+    private fun onLoginButtonPressed() {
+        val email: String = emailInputField.text.toString()
+        val password: String = passwordInputField.text.toString()
+
+        Authentication.signIn(email, password, { navigateToHome() })
+    }
+
+    private fun navigateToHome() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/grassyass/touchsomegrass/network/Authentication.kt
@@ -0,0 +1,38 @@
+package com.grassyass.touchsomegrass.network
+
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+object Authentication {
+    private val auth = Firebase.auth
+
+    fun createAccount(email: String, password: String,
+        onSuccess: OnSuccessListener<in AuthResult>? = null,
+        onFailure: OnFailureListener? = null) {
+
+        val task = auth.createUserWithEmailAndPassword(email, password)
+        if (onSuccess != null) { task.addOnSuccessListener(onSuccess) }
+        if (onFailure != null) { task.addOnFailureListener(onFailure)}
+    }
+
+    fun signIn(email: String, password: String,
+        onSuccess: OnSuccessListener<in AuthResult>? = null,
+        onFailure: OnFailureListener? = null) {
+
+        val task = auth.signInWithEmailAndPassword(email, password)
+        if (onSuccess != null) { task.addOnSuccessListener(onSuccess) }
+        if (onFailure != null) { task.addOnFailureListener(onFailure) }
+    }
+
+    fun signOut() {
+        auth.signOut()
+    }
+
+    fun getCurrentUser(): FirebaseUser? {
+        return auth.currentUser
+    }
+}
