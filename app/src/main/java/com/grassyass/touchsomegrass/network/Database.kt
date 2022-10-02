a15c1fc693ef9724fd0cd6842793f8f420f284c4
Anthony Aromin <menekoiskawaii@gmail.com>
2022-10-02 20:37:59 +0800

    Add Firebase Realtime Database

---
+++ app/src/main/java/com/grassyass/touchsomegrass/network/Database.kt
@@ -0,0 +1,29 @@
+package com.grassyass.touchsomegrass.network
+
+import com.google.android.gms.tasks.Task
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+object Database {
+    private const val dbURL: String = "https://touch-some-grass-default-rtdb.asia-southeast1.firebasedatabase.app"
+    private val dbRef: DatabaseReference = FirebaseDatabase.getInstance(dbURL).reference
+
+    fun writeData(path: String, data: Any) {
+        dbRef.child(path).setValue(data)
+    }
+
+    fun pushData(path: String, data: Any) {
+        val key: String? = dbRef.child(path).push().key
+
+        writeData("$path/$key", data)
+    }
+
+    fun updateChildren(updates: Map<String, Any>) {
+        dbRef.updateChildren(updates)
+    }
+
+    fun readData(path: String): Task<DataSnapshot> {
+        return dbRef.child(path).get()
+    }
+}
