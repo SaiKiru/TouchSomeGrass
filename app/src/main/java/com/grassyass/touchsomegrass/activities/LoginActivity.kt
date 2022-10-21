bb0198f4fda0f61f77617cb54bbd571ca3d30833
Anthony Aromin <menekoiskawaii@gmail.com>
2022-10-21 14:03:20 +0800

    Redesign Login Activity

--- app/src/main/java/com/grassyass/touchsomegrass/activities/LoginActivity.kt
+++ app/src/main/java/com/grassyass/touchsomegrass/activities/LoginActivity.kt
@@ -3,8 +3,11 @@ package com.grassyass.touchsomegrass.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import com.google.firebase.auth.FirebaseUser
 import com.grassyass.touchsomegrass.R
 import com.grassyass.touchsomegrass.network.Authentication
@@ -13,12 +16,23 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var emailInputField: EditText
     private lateinit var passwordInputField: EditText
     private lateinit var loginButton: Button
+    private lateinit var signUpButton: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        initializeUI()
+        emailInputField = findViewById(R.id.email_input_field)
+        passwordInputField = findViewById(R.id.password_input_field)
+        loginButton = findViewById(R.id.login_button)
+        signUpButton = findViewById(R.id.sign_up_button)
+
+        loginButton.setOnClickListener { onLoginButtonPressed() }
+        signUpButton.setOnClickListener { onSignUpButtonPressed() }
+
+        val spannable = SpannableString(signUpButton.text)
+        spannable.setSpan(UnderlineSpan(), 0, spannable.length, 0)
+        signUpButton.text = spannable
     }
 
     override fun onStart() {
@@ -28,14 +42,6 @@ class LoginActivity : AppCompatActivity() {
         if (currentUser != null) { navigateToHome() }
     }
 
-    private fun initializeUI() {
-        emailInputField = findViewById(R.id.email_input_field)
-        passwordInputField = findViewById(R.id.password_input_field)
-        loginButton = findViewById(R.id.login_button)
-
-        loginButton.setOnClickListener { onLoginButtonPressed() }
-    }
-
     private fun onLoginButtonPressed() {
         val email: String = emailInputField.text.toString()
         val password: String = passwordInputField.text.toString()
@@ -43,8 +49,17 @@ class LoginActivity : AppCompatActivity() {
         Authentication.signIn(email, password, { navigateToHome() })
     }
 
+    private fun onSignUpButtonPressed() {
+        navigateToSignUpScreen()
+    }
+
     private fun navigateToHome() {
         val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToSignUpScreen() {
+        val intent = Intent(this, SignUpActivity::class.java)
         startActivity(intent)
     }
 }
