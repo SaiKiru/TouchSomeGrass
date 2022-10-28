97a7fdba2a5c3200b19c6a8dd934f2bffb972d5a
Anthony Aromin <menekoiskawaii@gmail.com>
2022-10-28 22:38:10 +0800

    Add data overview in Home screen
    - User profile
    - Steps for the week

--- app/src/main/java/com/grassyass/touchsomegrass/fragments/HomeFragment.kt
+++ app/src/main/java/com/grassyass/touchsomegrass/fragments/HomeFragment.kt
@@ -1,34 +1,29 @@
 package com.grassyass.touchsomegrass.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import androidx.fragment.app.Fragment
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.grassyass.touchsomegrass.R
+import com.grassyass.touchsomegrass.activities.LoginActivity
+import com.grassyass.touchsomegrass.models.User
+import com.grassyass.touchsomegrass.network.Authentication
+import com.grassyass.touchsomegrass.network.api.UsersAPI
+import com.grassyass.touchsomegrass.views.BarGraph
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var userImageView: ImageView
+    private lateinit var userNameLabel: TextView
+    private lateinit var userLevelLabel: TextView
+    private lateinit var userTitleLabel: TextView
+    private lateinit var stepGraph: BarGraph
+    private lateinit var logOutButton: ImageButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +33,42 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        userImageView = view.findViewById(R.id.userImageView)
+        userNameLabel = view.findViewById(R.id.userNameLabel)
+        userLevelLabel = view.findViewById(R.id.userLevelLabel)
+        userTitleLabel = view.findViewById(R.id.userTitleLabel)
+        logOutButton = view.findViewById(R.id.log_out_button)
+        stepGraph = view.findViewById<FrameLayout>(R.id.step_graph).findViewById(R.id.graph)
+
+        logOutButton.setOnClickListener { onLogOutButtonPressed() }
+
+        stepGraph.setData(
+            arrayListOf(720, 2432, 640, 2201, 2670, 800, 343),
+            arrayListOf("S", "M", "T", "W", "T", "F", "S")
+        )
+
+        UsersAPI.getUser().addOnSuccessListener {
+            it.ref.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val user = snapshot.getValue(User::class.java)!!
+
+                    userNameLabel.text = user.name
                 }
-            }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // empty code
+                }
+            })
+        }
+    }
+
+    private fun onLogOutButtonPressed() {
+        Authentication.signOut()
+        val intent = Intent(context, LoginActivity::class.java)
+        startActivity(intent)
+        activity?.finish()
     }
 }
