1dfeaf5963d9d61b056d224847755937b71f6297
Charles-Rych <charlesdlp04@gmail.com>
2023-03-05 22:17:02 +0800

    Add monthly step stats and history

--- app/src/main/java/com/grassyass/touchsomegrass/fragments/HomeFragment.kt
+++ app/src/main/java/com/grassyass/touchsomegrass/fragments/HomeFragment.kt
@@ -30,7 +30,15 @@ class HomeFragment : Fragment() {
     private lateinit var stepGraph: BarGraph
     private lateinit var logOutButton: ImageButton
     private lateinit var editButton: ImageButton
+    private lateinit var weeklyStatsButton: Button
+    private lateinit var monthlyStatsButton: Button
+    private lateinit var timeSpanLabel: TextView
+    private lateinit var previousTimeSpanButton: ImageButton
+    private lateinit var nextTimeSpanButton: ImageButton
     private lateinit var user: User
+
+    private var overallStatsTimeRange: String = "WEEK"
+    private var overallStatsTimePointer: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,11 +57,21 @@ class HomeFragment : Fragment() {
         userTitleLabel = view.findViewById(R.id.userTitleLabel)
         logOutButton = view.findViewById(R.id.log_out_button)
         editButton = view.findViewById(R.id.edit_button)
+        weeklyStatsButton = view.findViewById(R.id.weeklyStatsButton)
+        monthlyStatsButton = view.findViewById(R.id.monthlyStatsButton)
+        timeSpanLabel = view.findViewById(R.id.timeSpanLabel)
+        previousTimeSpanButton = view.findViewById(R.id.previousTimeSpanButton)
+        nextTimeSpanButton = view.findViewById(R.id.nextTimeSpanButton)
         stepGraph = view.findViewById<FrameLayout>(R.id.step_graph).findViewById(R.id.graph)
 
+        weeklyStatsButton.setOnClickListener { onWeeklyStatsButtonPressed() }
+        monthlyStatsButton.setOnClickListener { onMonthlyStatsButtonPressed() }
+        previousTimeSpanButton.setOnClickListener { onPreviousTimeSpanButtonPressed() }
+        nextTimeSpanButton.setOnClickListener { onNextTimeSpanButtonPressed() }
         logOutButton.setOnClickListener { onLogOutButtonPressed() }
         editButton.setOnClickListener { onEditButtonPressed() }
 
+        updateTimeLabel()
         populateGraph()
 
         UsersAPI.getUser().addOnSuccessListener {
@@ -97,14 +115,106 @@ class HomeFragment : Fragment() {
         dialog.show(childFragmentManager, "edit_username_dialog")
     }
 
-    private fun populateGraph() {
+    private fun onWeeklyStatsButtonPressed() {
+        weeklyStatsButton.setTextColor(resources.getColor(R.color.white))
+        weeklyStatsButton.setBackgroundColor(resources.getColor(R.color.primary_400))
+
+        monthlyStatsButton.setTextColor(resources.getColor(R.color.primary_400))
+        monthlyStatsButton.setBackgroundColor(resources.getColor(R.color.white))
+
+        overallStatsTimeRange = "WEEK"
+        overallStatsTimePointer = 0
+        updateTimeLabel(overallStatsTimeRange, overallStatsTimePointer)
+        populateGraph(overallStatsTimeRange, overallStatsTimePointer)
+    }
+
+    private fun onMonthlyStatsButtonPressed() {
+        monthlyStatsButton.setTextColor(resources.getColor(R.color.white))
+        monthlyStatsButton.setBackgroundColor(resources.getColor(R.color.primary_400))
+
+        weeklyStatsButton.setTextColor(resources.getColor(R.color.primary_400))
+        weeklyStatsButton.setBackgroundColor(resources.getColor(R.color.white))
+
+        overallStatsTimeRange = "MONTH"
+        overallStatsTimePointer = 0
+        updateTimeLabel(overallStatsTimeRange, overallStatsTimePointer)
+        populateGraph(overallStatsTimeRange, overallStatsTimePointer)
+    }
+
+    private fun onNextTimeSpanButtonPressed() {
+        overallStatsTimePointer++
+        updateTimeLabel(overallStatsTimeRange, overallStatsTimePointer)
+        populateGraph(overallStatsTimeRange, overallStatsTimePointer)
+    }
+
+    private fun onPreviousTimeSpanButtonPressed() {
+        overallStatsTimePointer--
+        updateTimeLabel(overallStatsTimeRange, overallStatsTimePointer)
+        populateGraph(overallStatsTimeRange, overallStatsTimePointer)
+    }
+
+    private fun updateTimeLabel(span: String = "WEEK", pointer: Int = 0) {
         val cal = Calendar.getInstance()
 
-        cal.set(Calendar.DAY_OF_WEEK, cal.firstDayOfWeek)
-        val startTime = cal.timeInMillis.toDouble()
+        if (span == "WEEK") {
+            cal.set(Calendar.WEEK_OF_YEAR, cal.get(Calendar.WEEK_OF_YEAR) + pointer)
 
-        cal.set(Calendar.DAY_OF_WEEK, 7)
-        val endTime = cal.timeInMillis.toDouble()
+            cal.set(Calendar.DAY_OF_WEEK, cal.firstDayOfWeek)
+            val fromMonth = cal.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.US)
+            val fromDay = cal.get(Calendar.DAY_OF_MONTH)
+            val fromYear = cal.get(Calendar.YEAR)
+
+            cal.set(Calendar.DAY_OF_WEEK, cal.getActualMaximum(Calendar.DAY_OF_WEEK))
+            val toMonth = cal.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.US)
+            val toDay = cal.get(Calendar.DAY_OF_MONTH)
+            val toYear = cal.get(Calendar.YEAR)
+
+            if (fromYear != toYear) {
+                timeSpanLabel.text = "$fromMonth $fromDay $fromYear - $toMonth $toDay $toYear"
+            } else if (fromMonth != toMonth) {
+                timeSpanLabel.text = "$fromMonth $fromDay - $toMonth $toDay $toYear"
+            } else {
+                timeSpanLabel.text = "$fromMonth $fromDay - $toDay $toYear"
+            }
+        } else {
+            cal.set(Calendar.MONTH, cal.get(Calendar.MONTH) + (pointer * 4))
+
+            cal.set(Calendar.MONTH, cal.get(Calendar.MONTH) / 4 * 4)
+            val fromMonth = cal.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.US)
+
+            cal.set(Calendar.MONTH, cal.get(Calendar.MONTH) + 3)
+            val toMonth = cal.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.US)
+
+            val year = cal.get(Calendar.YEAR)
+
+            timeSpanLabel.text = "$fromMonth $year - $toMonth $year"
+        }
+    }
+
+    private fun populateGraph(span: String = "WEEK", pointer: Int = 0) {
+        val cal = Calendar.getInstance()
+        val startTime: Double
+        val endTime: Double
+
+        if (span === "WEEK") {
+            cal.set(Calendar.WEEK_OF_YEAR, cal.get(Calendar.WEEK_OF_YEAR) + pointer)
+
+            cal.set(Calendar.DAY_OF_WEEK, cal.firstDayOfWeek)
+            startTime = cal.timeInMillis.toDouble()
+
+            cal.set(Calendar.DAY_OF_WEEK, 7)
+            endTime = cal.timeInMillis.toDouble()
+        } else {
+            cal.set(Calendar.MONTH, cal.get(Calendar.MONTH) + (pointer * 4))
+
+            // This looks stupid but it is not
+            cal.set(Calendar.MONTH, cal.get(Calendar.MONTH) / 4 * 4)
+            startTime = cal.timeInMillis.toDouble()
+
+            cal.set(Calendar.MONTH, cal.get(Calendar.MONTH) + 3)
+            cal.set(Calendar.DAY_OF_MONTH, cal.getActualMaximum(Calendar.DAY_OF_MONTH))
+            endTime = cal.timeInMillis.toDouble()
+        }
 
         SessionsAPI.getSessions("_default").addOnSuccessListener {
             it.ref
@@ -113,18 +223,51 @@ class HomeFragment : Fragment() {
                 .endAt(endTime)
                 .addValueEventListener(object : ValueEventListener {
                     override fun onDataChange(snapshot: DataSnapshot) {
-                        val aggregate = arrayListOf<Any>(0, 0, 0, 0, 0, 0, 0)
+                        val aggregate: ArrayList<Any> =
+                            if (span == "WEEK") {
+                                arrayListOf(0, 0, 0, 0, 0, 0, 0)
+                            } else {
+                                arrayListOf(0, 0, 0, 0)
+                            }
 
                         snapshot.children.forEach { sessionSnapshot ->
                             val session = sessionSnapshot.getValue(Session::class.java)!!
 
                             cal.timeInMillis = session.end as Long
-                            val idx =  cal.get(Calendar.DAY_OF_WEEK)
 
-                            aggregate[idx - 1] = (aggregate[idx - 1] as Int) + (session.data as Long).toInt()
+                            val idx: Int =
+                                if (span == "WEEK") {
+                                    cal.get(Calendar.DAY_OF_WEEK) - 1
+                                } else {
+                                    cal.get(Calendar.MONTH) % 4
+                                }
+
+                            val data =
+                                if (session.data === null) 0L
+                                else session.data as Long
+
+                            aggregate[idx] = (aggregate[idx] as Int) + data.toInt()
                         }
 
-                        stepGraph.setData(aggregate, arrayListOf("S", "M", "T", "W", "T", "F", "S"))
+                        if (span == "WEEK") {
+                            stepGraph.setData(aggregate, arrayListOf("S", "M", "T", "W", "T", "F", "S"))
+                        } else {
+                            val cal2 = Calendar.getInstance()
+
+                            cal2.set(Calendar.MONTH, cal2.get(Calendar.MONTH) + (pointer * 4))
+
+                            cal2.set(Calendar.MONTH, cal2.get(Calendar.MONTH) / 4 * 4)
+                            val monthNames: ArrayList<String> = arrayListOf("", "", "", "")
+
+                            for (i in 0 until 4) {
+                                val monthName = cal2.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.US)
+                                cal2.set(Calendar.MONTH, cal2.get(Calendar.MONTH) + 1)
+
+                                monthNames[i] = monthName!!
+                            }
+
+                            stepGraph.setData(aggregate, monthNames)
+                        }
                     }
 
                     override fun onCancelled(error: DatabaseError) { }
