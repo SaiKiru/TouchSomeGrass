3cb4f6cf5f9e5cc51f8c90b61eb3ff4fa838b051
Charles-Rych <charlesdlp04@gmail.com>
2023-07-17 17:06:51 +0800

    Edit exercise description

--- app/src/main/java/com/grassyass/touchsomegrass/adapters/ExerciseListAdapter.kt
+++ app/src/main/java/com/grassyass/touchsomegrass/adapters/ExerciseListAdapter.kt
@@ -27,7 +27,15 @@ class ExerciseListAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = exerciseList[position]
 
+        val descriptionText =
+            if (item.type == Exercise.ExerciseType.DurationExercise) {
+                "Target: ${item.target as Long / (1_000L * 60)} minutes"
+            } else {
+                "Target: ${item.target} steps"
+            }
+
         holder.exerciseNameLabel.text = item.name
+        holder.exerciseDescription.text = descriptionText
         holder.setOnClickListener {
             Intent(context, ExerciseSettingsActivity::class.java).also {
                 it.putExtra(context.getString(R.string.extra_edit_exercise), item)
@@ -42,6 +50,7 @@ class ExerciseListAdapter(
 
     class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val exerciseNameLabel: TextView = view.findViewById(R.id.exercise_name_label)
+        val exerciseDescription: TextView = view.findViewById(R.id.exercise_description)
 
         fun setOnClickListener(onClickListener: OnClickListener) {
             view.setOnClickListener(onClickListener)
